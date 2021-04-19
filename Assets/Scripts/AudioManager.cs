d0a0db3606eee1904f288f3f947dbd02cb49632e
Vyacheslav Vasilev <vyach.vasilev@gmail.com>
2021-04-19 08:39:06 +0300

    minor update

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,18 +2,31 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    private static AudioManager Instance;
+
     private AudioSource _audio;
 
     [SerializeField] private AudioClip _gameOver;
     [SerializeField] private AudioClip _clearLevel;
     [SerializeField] private AudioClip _dropDown;
+
     public AudioClip GameOver => _gameOver;
     public AudioClip ClearLevel => _clearLevel;
     public AudioClip DropDown => _dropDown;
-    
 
     private void Awake()
     {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(this);
+
         _audio = GetComponent<AudioSource>();
     }
 
