f80cd90585982f46237ce3745d61a708e5aade6c
Viacheslav Vasiliev <viacheslav.vasiliev@plarium.com>
2021-04-21 17:25:57 +0300

    fix L-block rotaion

--- Assets/Scripts/TileContainer.cs
+++ Assets/Scripts/TileContainer.cs
@@ -4,4 +4,15 @@ public class TileContainer: MonoBehaviour
 {
     [SerializeField] private Vector3[] _positions;
     public Vector3[] Positions => _positions;
+
+    #if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        foreach (var position in _positions)
+        {
+            Gizmos.DrawWireCube(position, Vector3.one);
+        }
+    }
+    #endif
 }
