cbf1dd3f2c616b6bde5d9849369780ee39e47071
Vyacheslav Vasilev <vyach.vasilev@gmail.com>
2021-04-28 16:36:32 +0300

    add mute sound setting

---
+++ Assets/Scripts/Helper/MuteSetting.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MuteSetting : MonoBehaviour
+{
+    private Toggle _toggle;
+    
+    private void Awake()
+    {
+        _toggle = GetComponent<Toggle>();
+        _toggle.isOn = LoadSoundMode().Equals(0);
+    }
+
+    public void SetSoundMode()
+    {
+        AudioListener.volume = !_toggle.isOn ? 1 : 0;
+        SaveSoundMode();
+    }
+
+    private void SaveSoundMode()
+    {
+        PlayerPrefs.SetFloat("SoundMode", AudioListener.volume);
+    }
+
+    private float LoadSoundMode()
+    {
+        AudioListener.volume = PlayerPrefs.GetFloat("SoundMode");
+        return AudioListener.volume;
+    }
+}
