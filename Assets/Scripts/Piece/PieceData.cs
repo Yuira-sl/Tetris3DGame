71f8dc04fb4be4a2c53a950d470bab91bf91e429
Vyacheslav Vasilev <vyach.vasilev@gmail.com>
2021-05-06 19:34:34 +0300

    add piece data

---
+++ Assets/Scripts/Piece/PieceData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Octamino
+{
+    [CreateAssetMenu()]
+    public class PieceData : ScriptableObject
+    {
+        public GameObject Block;
+        public List<Material> PieceMaterials = new List<Material>();
+        public Material GhostPieceMaterial;
+    }
+}
